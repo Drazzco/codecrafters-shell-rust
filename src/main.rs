a752593cca9cdfd10f91f574d60b43d62e4922be
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-05-30 11:34:15 -0300

    type builtin: executable files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
+use std::env;
 use std::io::{self, Write};
+use std::path::Path;
 use std::process;
 
 fn main() {
@@ -20,7 +22,21 @@ fn main() {
             "type" => if tokens[1] == "exit" || tokens[1] == "echo" || tokens[1] == "type" {
                 println!("{} is a shell builtin", tokens[1])
             } else {
-                println!("{} not found", tokens[1])
+                let path_var = env::var("PATH").unwrap_or_default();
+                let paths: Vec<&str> = path_var.split(':').collect();
+                let mut found = false;
+                for path in paths {
+                    let mut full_path = Path::new(path).join(tokens[1]);
+                    full_path.set_extension("");
+                    if full_path.exists() {
+                        println!("{} is {}", tokens[1], full_path.display());
+                        found = true;
+                        break;
+                    }
+                }
+                if !found {
+                    println!("{} not found", tokens[1]);
+                }
             }
             _ => println!("{}: command not found", command),
         }
